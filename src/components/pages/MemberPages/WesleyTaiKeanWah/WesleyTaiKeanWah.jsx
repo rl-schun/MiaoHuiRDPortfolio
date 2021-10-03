661f00c46b894013d016aa6e890317c28a6e3376
Wesley <taikeanwah.wesley@gmail.com>
2021-10-03 18:27:16 +1100

    Added personal style sheet for testing purpose

--- src/components/pages/MemberPages/WesleyTaiKeanWah/WesleyTaiKeanWah.jsx
+++ src/components/pages/MemberPages/WesleyTaiKeanWah/WesleyTaiKeanWah.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {Testing} from "./WesleyTaiKeanWah.style";
 
 function WesleyTaiKeanWah() {
   return <div>
     <h1>Ola I'm Wesley Tai!!!</h1>
+    <Testing>Hello</Testing>
     <h2>A Research Engineer from Melbourne who knows basic programming.</h2>
     <h2>What's up yall</h2>
+    <h2>You guys are the best</h2>
   </div>;
 }
 
---
+++ src/components/pages/MemberPages/WesleyTaiKeanWah/WesleyTaiKeanWah.style.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components'
+export const Testing = styled.h1`
+color: #FF0000
+`
